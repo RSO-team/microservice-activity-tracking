60e0afebfa3816d3e0cef1d410b4bfc662618b24
tstrus <tstrusplume.com>
2021-12-31 15:13:16 +0100

    delete

--- push-to-dockerhub.bat
+++
@@ -1,3 +0,0 @@
-docker build -t lgaljo/rt_basketball_activity_tracking -f Dockerfile_with_maven_build .
-docker tag lgaljo/rt_basketball_activity_tracking lgaljo/rt_basketball_activity_tracking:latest
-docker push -a lgaljo/rt_basketball_activity_tracking
